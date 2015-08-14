5f56400d3487bba1c5b6baf2ac5e65d415b47feb
Alexandre Mertens <alexandre.mertens@cern.ch>
2015-08-14 10:16:07 +0200

    adding some new candidates

--- interface/TTAnalyzer.h
+++ interface/TTAnalyzer.h
@@ -16,19 +16,22 @@ class TTAnalyzer: public Framework::Analyzer {
 
         virtual void analyze(const edm::Event&, const edm::EventSetup&, const ProducersManager&) override;
 
+/*
         virtual void registerCategories(CategoryManager& manager) {
             manager.new_category("mumu", "Category with leading leptons as two muons", &mumu_category);
             manager.new_category("elel", "Category with leading leptons as two electrons", &elel_category);
             manager.new_category("muel", "Category with leading leptons as muon, electron", &muel_category);
             manager.new_category("elmu", "Category with leading leptons as electron, muon", &elmu_category);
-        }
 
+        }
+*/
     private:
+/*
         MuMuCategory mumu_category;
         ElElCategory elel_category;
         MuElCategory muel_category;
         ElMuCategory elmu_category;
-
+*/
 };
 
 #endif
--- plugins/TTAnalyzer.cc
+++ plugins/TTAnalyzer.cc
@@ -1,6 +1,36 @@
 #include <cp3_llbb/TTAnalysis/interface/TTAnalyzer.h>
 
+#include <cp3_llbb/Framework/interface/GenParticlesProducer.h>
+#include <cp3_llbb/Framework/interface/JetsProducer.h>
+
 void TTAnalyzer::analyze(const edm::Event& event, const edm::EventSetup& setup, const ProducersManager& producers) {
+
+
+    std::cout << " Starting an analysis " << std::endl;
+
+    const JetsProducer& jets = dynamic_cast<const JetsProducer&>(producers.get("jets"));
+    const ElectronsProducer& electrons = dynamic_cast<const ElectronsProducer&>(producers.get("electrons"));
+    const MuonsProducer& muons = dynamic_cast<const MuonsProducer&>(producers.get("muons"));
+
+    BRANCH(bb, LorentzVector);
+    BRANCH(ee, LorentzVector);
+    BRANCH(mm, LorentzVector);
+
+    bb.SetPxPyPzE(0.,0.,0.,0.);
+    ee.SetPxPyPzE(0.,0.,0.,0.);
+    mm.SetPxPyPzE(0.,0.,0.,0.);
+
+    if (jets.p4.size() > 1) {
+        bb = jets.p4.at(0)+jets.p4.at(1);
+    }
+
+    if (electrons.p4.size() > 1) {
+        ee = electrons.p4.at(0)+electrons.p4.at(1);
+    }
+    
+    if (muons.p4.size() > 1) {
+        mm = muons.p4.at(0)+muons.p4.at(1);
+    }
 
 }
 
