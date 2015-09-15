5f0f6a9f0c652123c53ed317b455f8a8730edb1d
Sébastien <sebastien.wertz@uclouvain.be>
2015-09-15 10:49:57 +0200

    Removed dilepton categories, updated python config

--- interface/TTAnalyzer.h
+++ interface/TTAnalyzer.h
@@ -2,10 +2,6 @@
 #define TTANALYZER_H
 
 #include <cp3_llbb/Framework/interface/Analyzer.h>
-#include <cp3_llbb/Framework/interface/MuonsProducer.h>
-#include <cp3_llbb/Framework/interface/ElectronsProducer.h>
-
-#include <cp3_llbb/TTAnalysis/interface/Categories.h>
 
 class TTAnalyzer: public Framework::Analyzer {
     public:
@@ -14,20 +10,9 @@ class TTAnalyzer: public Framework::Analyzer {
 
         }
 
-        virtual void analyze(const edm::Event&, const edm::EventSetup&, const ProducersManager&) override;
-
-        virtual void registerCategories(CategoryManager& manager) {
-            manager.new_category("mumu", "Category with leading leptons as two muons", &mumu_category);
-            manager.new_category("elel", "Category with leading leptons as two electrons", &elel_category);
-            manager.new_category("muel", "Category with leading leptons as muon, electron", &muel_category);
-            manager.new_category("elmu", "Category with leading leptons as electron, muon", &elmu_category);
-        }
+        virtual void analyze(const edm::Event&, const edm::EventSetup&, const ProducersManager&, const CategoryManager&) override;
 
     private:
-        MuMuCategory mumu_category;
-        ElElCategory elel_category;
-        MuElCategory muel_category;
-        ElMuCategory elmu_category;
 
 };
 
---
+++ test/TTConfigurationMC.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+from cp3_llbb.Framework import Framework
+
+process = Framework.create(False, eras.Run2_25ns, '74X_mcRun2_asymptotic_v2', cms.PSet(
+    dilepton = cms.PSet(
+        type = cms.string('dilepton_analyzer'),
+        prefix = cms.string('dilepton_'),
+        enable = cms.bool(True),
+        categories_parameters = cms.PSet(
+            mll_cut = cms.untracked.double(20)
+            ),
+        parameters = cms.PSet(
+            standalone = cms.untracked.bool(True),
+            muons_wp = cms.untracked.string('loose'),
+            electrons_wp = cms.untracked.string('loose')
+            )
+        ),
+
+    tt = cms.PSet(
+        type = cms.string('tt_analyzer'),
+        prefix = cms.string('tt_'),
+        enable = cms.bool(True)
+        )
+    ), redoJEC=True
+    )
+
+Framework.schedule(process, ['dilepton', 'tt'])
+
+process.source.fileNames = cms.untracked.vstring(
+        'file:///home/fynu/sbrochet/storage/MINIAODSIM/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_Asympt25ns_MCRUN2_74_V9_reduced.root'
+        )
+
