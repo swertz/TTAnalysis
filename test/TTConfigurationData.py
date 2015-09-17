6b7a52ad071534129fa1e2bfd140e016951dc79c
Sébastien <sebastien.wertz@uclouvain.be>
2015-09-17 13:50:02 +0200

    Added b-jet analyzers to data config

--- test/TTConfigurationData.py
+++ test/TTConfigurationData.py
@@ -62,7 +62,7 @@ process = Framework.create(True, eras.Run2_50ns, '74X_dataRun2_v2', cms.PSet(
     ), redoJEC=True
     )
 
-Framework.schedule(process, ['dilepton', 'tt'])
+Framework.schedule(process, ['bTagsLoose', 'bTagsMedium', 'bTagsTight', 'tt'])
 
 process.source.fileNames = cms.untracked.vstring(
         '/store/data/Run2015B/DoubleMuon/MINIAOD/17Jul2015-v1/30000/D8ED75E7-C12E-E511-8CBF-0025905A608C.root' 
