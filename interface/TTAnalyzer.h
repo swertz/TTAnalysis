c4ea4b7195d6e5bd12332b0c984db5c553dfba4b
Sébastien <sebastien.wertz@uclouvain.be>
2015-10-06 10:21:48 +0200

    New lepton storing

--- interface/TTAnalyzer.h
+++ interface/TTAnalyzer.h
@@ -4,68 +4,155 @@
 #include <cp3_llbb/Framework/interface/MuonsProducer.h>
 #include <cp3_llbb/Framework/interface/Analyzer.h>
 
+struct Lepton {
+  LorentzVector p4;
+  uint8_t idx;
+  bool isMu;
+  bool isEl;
+}
+
+struct DiLepton {
+  LorentzVector p4;
+  std::pair<int, int> idxs;
+  bool isElEl;
+  bool isElMu;
+  bool isMuEl;
+  bool isMuMu;
+}
+
+struct DiJet {
+  LorentzVector p4;
+  std::pair<uint8_t, uint8_t> idxs;
+}
+
 class TTAnalyzer: public Framework::Analyzer {
     public:
         TTAnalyzer(const std::string& name, const ROOT::TreeGroup& tree_, const edm::ParameterSet& config):
             Analyzer(name, tree_, config),
             
-            m_electronIsoCut( config.getUntrackedParameter<double>("electronIsoCut", 10.) ),
-            m_electronEtaCut( config.getUntrackedParameter<double>("electronEtaCut") ),
             m_electronPtCut( config.getUntrackedParameter<double>("electronPtCut") ),
-            m_electron_veto_wp_name( config.getUntrackedParameter<std::string>("electron_veto_wp_name") ),
-            m_electron_loose_wp_name( config.getUntrackedParameter<std::string>("electron_loose_wp_name") ),
-            m_electron_medium_wp_name( config.getUntrackedParameter<std::string>("electron_medium_wp_name") ),
-            m_electron_tight_wp_name( config.getUntrackedParameter<std::string>("electron_tight_wp_name") ),
-            m_electron_selectedID_name( config.getUntrackedParameter<std::string>("electron_selectedID_name") ),
+            m_electronEtaCut( config.getUntrackedParameter<double>("electronEtaCut") ),
+            m_electronVetoIDName( config.getUntrackedParameter<std::string>("electronVetoIDName") ),
+            m_electronLooseIDName( config.getUntrackedParameter<std::string>("electronLooseIDName") ),
+            m_electronMediumIDName( config.getUntrackedParameter<std::string>("electronMediumIDName") ),
+            m_electronTightIDName( config.getUntrackedParameter<std::string>("electronTightIDName") ),
+            m_electronSelectedIDName( config.getUntrackedParameter<std::string>("electronSelectedIDName") ),
             
-            m_muonIsoCut( config.getUntrackedParameter<double>("muonIsoCut") ),
-            m_muonEtaCut( config.getUntrackedParameter<double>("muonEtaCut") ),
             m_muonPtCut( config.getUntrackedParameter<double>("muonPtCut") ),
-            m_muon_selectedID_wp( config.getUntrackedParameter<std::string>("muon_selectedID_wp") ),
+            m_muonEtaCut( config.getUntrackedParameter<double>("muonEtaCut") ),
+            m_muonBaseIsoCut( config.getUntrackedParameter<double>("muonBaseIsoCut") ),
+            m_muonSelectedIsoCut( config.getUntrackedParameter<double>("muonSelectedIsoCut") ),
+            m_muonSelectedID( config.getUntrackedParameter<std::string>("muonSelectedID") ),
             
-            m_jetEtaCut( config.getUntrackedParameter<double>("jetEtaCut") ),
+            m_MllBaseCutSF( config.getUntrackedParameter<double>("MllBaseCutSF", 0.) ),
+            m_MllBaseCutDF( config.getUntrackedParameter<double>("MllBaseCutDF", 0.) ),
+            
             m_jetPtCut( config.getUntrackedParameter<double>("jetPtCut") )
+            m_jetEtaCut( config.getUntrackedParameter<double>("jetEtaCut") ),
+            m_jetPUID( config.getUntrackedParameter<double>("jetPUID", -1000) ),
+            m_jetID( config.getUntrackedParameter<std::string>("jetID") ),
+            
+            m_hltEtaCut( config.getUntrackedParameter<double>("hltEtaCut") ),
+            m_hltPtCut( config.getUntrackedParameter<double>("hltPtCut", 999999) ),
         {
         }
 
         virtual void analyze(const edm::Event&, const edm::EventSetup&, const ProducersManager&, const CategoryManager&) override;
         virtual void registerCategories(CategoryManager& manager, const edm::ParameterSet&) override;
 
-        BRANCH(selectedElectrons, std::vector<uint8_t>);
+        BRANCH(selectedElectrons, std::vector<uint8_t>); // index points to electrons array
         BRANCH(isolatedElectrons, std::vector<uint8_t>);
         BRANCH(tightElectrons, std::vector<uint8_t>);
         BRANCH(looseElectrons, std::vector<uint8_t>);
         BRANCH(mediumElectrons, std::vector<uint8_t>);
         BRANCH(vetoElectrons, std::vector<uint8_t>);
         
-        BRANCH(selectedMuons, std::vector<uint8_t>);
-        BRANCH(isolatedMuons, std::vector<uint8_t>);
+        BRANCH(selectedMuons, std::vector<uint8_t>); // index points to muons array
         BRANCH(tightMuons, std::vector<uint8_t>);
         BRANCH(mediumMuons, std::vector<uint8_t>);
         BRANCH(looseMuons, std::vector<uint8_t>);
 
-        BRANCH(selectedLeadingMuMu, std::pair<int, int>);
-        BRANCH(selectedLeadingMuEl, std::pair<int, int>);
-        BRANCH(selectedLeadingElMu, std::pair<int, int>);
-        BRANCH(selectedLeadingElEl, std::pair<int, int>);
+        BRANCH(leadingSelectedMuMu, std::pair<int, int>); // indices point to electrons/muons arrays
+        BRANCH(leadingSelectedMuEl, std::pair<int, int>);
+        BRANCH(leadingSelectedElMu, std::pair<int, int>);
+        BRANCH(leadingSelectedElEl, std::pair<int, int>);
         
         BRANCH(selectedJets, std::vector<uint8_t>);
+        BRANCH(selectedJets_diBJetExcluded, std::vector<uint8_t>);
+        BRANCH(selectedBJets_CSVv2L, std::vector<uint8_t>);
+        BRANCH(selectedBJets_CSVv2M, std::vector<uint8_t>);
+        BRANCH(selectedBJets_CSVv2T, std::vector<uint8_t>);
+
+        BRANCH(lepton_p4, std::vector<LorentzVector>);
+        BRANCH(lepton_idx, std::vector<uint8_t>); // index points to electrons/muons arrays
+        BRANCH(lepton_isMu, std::vector<bool>);
+        BRANCH(lepton_isEl, std::vector<bool>);
+        
+        BRANCH(diLepton_p4, LorentzVector);
+        BRANCH(diLepton_idxs, std::pair<uint8_t, uint8_t>); // index points to lepton array
+        BRANCH(diLepton_isMuMu, bool);
+        BRANCH(diLepton_isMuEl, bool);
+        BRANCH(diLepton_isElMu, bool);
+        BRANCH(diLepton_isElEl, bool);
+        BRANCH(diLepton_DR, float);
+        BRANCH(diLepton_DPhi, float);
+        BRANCH(diLepton_DEta, float);
+
+        BRANCH(diJet_p4, LorentzVector);
+        BRANCH(diJet_idxs, std::pair<uint8_t, uint8_t>);
+        BRANCH(diJet_DR, float);
+        BRANCH(diJet_DPhi, float);
+        BRANCH(diJet_DEta, float);
+
+        BRANCH(diBJet_p4, LorentzVector);
+        BRANCH(diBJet_idxs, std::pair<uint8_t, uint8_t>);
+        BRANCH(diBJet_DR, float);
+        BRANCH(diBJet_DPhi, float);
+        BRANCH(diBJet_DEta, float);
+
+        BRANCH(ll_jj_p4, LorentzVector);
+        BRANCH(ll_jj_DR, float);
+        BRANCH(ll_jj_DPhi, float);
+        BRANCH(ll_jj_DEta, float);
+        BRANCH(lj_minDR, float);
+
+        BRANCH(ll_bb_p4, LorentzVector);
+        BRANCH(ll_bb_DR, float);
+        BRANCH(ll_bb_DPhi, float);
+        BRANCH(ll_bb_DEta, float);
+        BRANCH(lb_minDR, float);
+
+        BRANCH(ll_jj_pfMET_p4, LorentzVector);
+        BRANCH(ll_bb_pfMET_p4, LorentzVector);
 
     private:
 
-        const float m_electronIsoCut, m_electronEtaCut, m_electronPtCut;
-        const std::string m_electron_veto_wp_name;
-        const std::string m_electron_loose_wp_name;
-        const std::string m_electron_medium_wp_name;
-        const std::string m_electron_tight_wp_name;
-        const std::string m_electron_selectedID_name;
+        const float m_electronPtCut, m_electronEtaCut;
+        const std::string m_electronVetoIDName;
+        const std::string m_electronLooseIDName;
+        const std::string m_electronMediumIDName;
+        const std::string m_electronTightIDName;
+        const std::string m_electronSelectedIDName;
 
-        const float m_muonIsoCut, m_muonEtaCut, m_muonPtCut;
-        const std::string m_muon_selectedID_wp;
+        const float m_muonPtCut, m_muonEtaCut, m_muonBaseIsoCut, m_muonSelectedIsoCut;
+        const std::string m_muonSelectedID;
 
-        const float m_jetEtaCut, m_jetPtCut;
+        const float m_MllBaseCutSF, m_MllBaseCutDF;
 
-        static inline bool muonIDAccessor(const MuonsProducer& muons, const uint8_t index, const std::string muonID){
+        const float m_jetPtCut, m_jetEtaCut, m_jetPUID;
+        const std::string m_jetID;
+        const float m_bJetCSVv2L, m_bJetCSVv2M, m_bJetCSVv2T;
+
+        const float m_hltEtaCut, m_hltPtCut;
+
+        std::vector<Lepton> m_leptons;
+        DiLepton m_diLepton;
+        DiJet m_diJet, m_diBJet_PtChosen, m_diBJet_DiscrChosen;
+        
+        static inline bool muonIDAccessor(const MuonsProducer& muons, const uint8_t index, const std::string& muonID){
+            if(index >= muons.p4.size())
+              throw edm::Exception(edm::errors::StdException, "Invalid muon index passed to ID accessor");
+
             if(muonID == "loose")
                 return muons.isLoose[index];
             
@@ -76,6 +163,22 @@ class TTAnalyzer: public Framework::Analyzer {
                 return muons.isTight[index];
             
             throw edm::Exception(edm::errors::NotFound, "Unknown muonID passed to analyzer");
+        }
+        
+        static inline bool jetIDAccessor(const JetsProducer& jets, const uint8_t index, const std::string& jetID){
+            if(index >= jets.p4.size())
+              throw edm::Exception(edm::errors::StdException, "Invalid jet index passed to ID accessor");
+            
+            if(jetID == "loose")
+                return jets.passLooseID[index];
+            
+            if(jetID == "tight")
+                return jets.passTightID[index];
+            
+            if(jetID == "tightLeptonVeto")
+                return jets.passTightLeptonVetoID[index];
+            
+            throw edm::Exception(edm::errors::NotFound, "Unknown jetID passed to analyzer");
         }
 };
 
--- plugins/TTAnalyzer.cc
+++ plugins/TTAnalyzer.cc
@@ -8,92 +8,159 @@
 
 void TTAnalyzer::analyze(const edm::Event& event, const edm::EventSetup& setup, const ProducersManager& producers, const CategoryManager& categories) {
 
+    ///////////////////////////
+    //       ELECTRONS       //
+    ///////////////////////////
+
     const ElectronsProducer& electrons = producers.get<ElectronsProducer>("electrons");
-    const MuonsProducer& muons = producers.get<MuonsProducer>("muons");
 
     for(unsigned int ielectron = 0; ielectron < electrons.p4.size(); ielectron++){
-        if( electrons.ids[ielectron][m_electron_veto_wp_name] )
+        if( electrons.ids[ielectron][m_electronVetoIDName] && electrons.p4[ielectron].Pt() > m_electronPtCut && abs(electrons.p4[ielectron].Eta()) < m_electronEtaCut )
             vetoElectrons.push_back(ielectron);
-        if( electrons.ids[ielectron][m_electron_loose_wp_name] )
+        if( electrons.ids[ielectron][m_electronLooseIDName] && electrons.p4[ielectron].Pt() > m_electronPtCut && abs(electrons.p4[ielectron].Eta()) < m_electronEtaCut )
             looseElectrons.push_back(ielectron);
-        if( electrons.ids[ielectron][m_electron_medium_wp_name] )
+        if( electrons.ids[ielectron][m_electronMediumIDName] && electrons.p4[ielectron].Pt() > m_electronPtCut && abs(electrons.p4[ielectron].Eta()) < m_electronEtaCut )
             mediumElectrons.push_back(ielectron);
-        if( electrons.ids[ielectron][m_electron_tight_wp_name] )
+        if( electrons.ids[ielectron][m_electronTightIDName] && electrons.p4[ielectron].Pt() > m_electronPtCut && abs(electrons.p4[ielectron].Eta()) < m_electronEtaCut )
             tightElectrons.push_back(ielectron);
-        if( electrons.relativeIsoR03_withEA[ielectron] < m_electronIsoCut )
-            isolatedElectrons.push_back(ielectron);
 
-        if( electrons.ids[ielectron][m_electron_selectedID_name] && electrons.relativeIsoR03_withEA[ielectron] < m_electronIsoCut && electrons.p4[ielectron].Pt() > m_electronPtCut && abs(electrons.p4[ielectron].Eta()) < m_electronEtaCut )
+        if( electrons.ids[ielectron][m_electronSelectedIDName] && electrons.p4[ielectron].Pt() > m_electronPtCut && abs(electrons.p4[ielectron].Eta()) < m_electronEtaCut ){
             selectedElectrons.push_back(ielectron);
+            m_leptons.push_back( { electrons.p4[ielectron], ielectron, true, false } );
+        }
     }
+
+    ///////////////////////////
+    //       MUONS           //
+    ///////////////////////////
+    
+    const MuonsProducer& muons = producers.get<MuonsProducer>("muons");
 
     for(unsigned int imuon = 0; imuon < muons.p4.size(); imuon++){
-        if( muons.isLoose[imuon] )
+        if(muons.relativeIsoR04_withEA[imuon] < m_muonBaseIsoCut &&  muons.isLoose[imuon] && muons.p4[imuon].Pt() > m_muonPtCut && abs(muons.p4[imuon].Eta()) < m_muonEtaCut )
             looseMuons.push_back(imuon);
-        if( muons.isMedium[imuon] )
+        if(muons.relativeIsoR04_withEA[imuon] < m_muonBaseIsoCut &&  muons.isMedium[imuon] && muons.p4[imuon].Pt() > m_muonPtCut && abs(muons.p4[imuon].Eta()) < m_muonEtaCut )
             mediumMuons.push_back(imuon);
-        if( muons.isTight[imuon] )
+        if(muons.relativeIsoR04_withEA[imuon] < m_muonBaseIsoCut &&  muons.isTight[imuon] && muons.p4[imuon].Pt() > m_muonPtCut && abs(muons.p4[imuon].Eta()) < m_muonEtaCut )
             tightMuons.push_back(imuon);
-        if( muons.relativeIsoR04_withEA[imuon] < m_muonIsoCut )
-            isolatedMuons.push_back(imuon);
 
-        if(muonIDAccessor(muons, imuon, m_muon_selectedID_wp) && muons.relativeIsoR04_withEA[imuon] < m_muonIsoCut && muons.p4[imuon].Pt() > m_muonPtCut && abs(muons.p4[imuon].Eta()) < m_muonEtaCut )
+        if(muonIDAccessor(muons, imuon, m_muon_selectedID) && muons.relativeIsoR04_withEA[imuon] < m_muonSelectedIsoCut && muons.p4[imuon].Pt() > m_muonPtCut && abs(muons.p4[imuon].Eta()) < m_muonEtaCut ){
             selectedMuons.push_back(imuon);
+            m_leptons.push_back( { muons.p4[imuon], imuon, false, true } );
+        }
     }
 
-    // Find the highest-Pt opposite-charge pairs for each couple of flavours, on selected objets only
-    selectedLeadingElEl = std::make_pair(-1, -1);
-    selectedLeadingElMu = std::make_pair(-1, -1);
-    selectedLeadingMuEl = std::make_pair(-1, -1);
-    selectedLeadingMuMu = std::make_pair(-1, -1);
+    // Sort the m_leptons vector according to Pt and write the content to disk
+    std::sort(m_leptons.begin(), m_leptons.end(), [](Lepton a, Lepton b){ return b.p4.Pt() > a.p4.Pt(); });
+    for(const Lepton lepton&: m_leptons){
+      lepton_p4.push_back(lepton.p4);
+      lepton_idx.push_back(lepton.idx);
+      lepton_isMu.push_back(lepton.isMu);
+      lepton_isEl.push_back(lepton.isEl);
+    }
+
+    ///////////////////////////
+    //       DILEPTONS       //
+    ///////////////////////////
+
+    // First find the highest-Pt opposite-charge pairs for each couple of flavours, on selected objets only
+    // Already apply a cut on Mll
+    
+    leadingSelectedElEl = std::make_pair(-1, -1);
+    leadingSelectedElMu = std::make_pair(-1, -1);
+    leadingSelectedMuEl = std::make_pair(-1, -1);
+    leadingSelectedMuMu = std::make_pair(-1, -1);
+
+    float tempSumPtElEl = 0, tempSumPtElMu = 0, tempSumPtMuEl = 0, tempSumPtMuMu = 0; 
 
     // El-El
     for(unsigned int iele1 = 0; iele1 < selectedElectrons.size(); iele1++){
         for(unsigned int iele2 = iele1+1; iele2 < selectedElectrons.size(); iele2++){
-            if( electrons.charge[ selectedElectrons[iele1] ] * electrons.charge[ selectedElectrons[iele2] ] < 0 ){
-                selectedLeadingElEl = std::make_pair(iele1, iele2);
+            if( electrons.charge[ selectedElectrons[iele1] ] * electrons.charge[ selectedElectrons[iele2] ] < 0 && (electrons.p4[ selectedElectrons[iele1] ] + electrons.p4[ selectedElectrons[iele2] ]).M() > m_MllBaseCutSF ){
+                leadingSelectedElEl = std::make_pair(iele1, iele2);
+                tempSumPtElEl = electrons.p4[ selectedElectrons[iele1] ].Pt() + electrons.p4[ selectedElectrons[iele2] ].Pt(); 
                 break;
             }
         }
-        if(selectedLeadingElEl.first >= 0)
+        if(leadingSelectedElEl.first >= 0)
             break;
     }
 
     // El-Mu
     for(unsigned int iele = 0; iele < selectedElectrons.size(); iele++){
         for(unsigned int imu = 0; imu < selectedMuons.size(); imu++){
-            if( electrons.p4[ selectedElectrons[iele] ].Pt() > muons.p4[ selectedMuons[imu] ].Pt() && electrons.charge[ selectedElectrons[iele] ] * muons.charge[ selectedMuons[imu] ] < 0 ){
-                selectedLeadingElMu = std::make_pair(iele, imu);
+            if( electrons.p4[ selectedElectrons[iele] ].Pt() > muons.p4[ selectedMuons[imu] ].Pt() && electrons.charge[ selectedElectrons[iele] ] * muons.charge[ selectedMuons[imu] ] < 0 && (electrons.p4[ selectedElectrons[iele] ] + muons.p4[ selectedMuons[imu] ]).M() > m_MllBaseCutDF){
+                leadingSelectedElMu = std::make_pair(iele, imu);
+                tempSumPtElMu = electrons.p4[ selectedElectrons[iele] ].Pt() + muons.p4[ selectedMuons[imu] ].Pt(); 
                 break;
             }
         }
-        if(selectedLeadingElMu.first >= 0)
+        if(leadingSelectedElMu.first >= 0)
             break;
     }
     
     // Mu-El
     for(unsigned int imu = 0; imu < selectedMuons.size(); imu++){
         for(unsigned int iele = 0; iele < selectedElectrons.size(); iele++){
-            if( electrons.p4[ selectedElectrons[iele] ].Pt() < muons.p4[ selectedMuons[imu] ].Pt() && electrons.charge[ selectedElectrons[iele] ] * muons.charge[ selectedMuons[imu] ] < 0 ){
-                selectedLeadingMuEl = std::make_pair(imu, iele);
+            if( electrons.p4[ selectedElectrons[iele] ].Pt() < muons.p4[ selectedMuons[imu] ].Pt() && electrons.charge[ selectedElectrons[iele] ] * muons.charge[ selectedMuons[imu] ] < 0 && (electrons.p4[ selectedElectrons[iele] ] + muons.p4[ selectedMuons[imu] ]).M() > m_MllBaseCutDF){
+                leadingSelectedMuEl = std::make_pair(imu, iele);
+                tempSumPtMuEl = electrons.p4[ selectedElectrons[iele] ].Pt() + muons.p4[ selectedMuons[imu] ].Pt(); 
                 break;
             }
         }
-        if(selectedLeadingMuEl.first >= 0)
+        if(leadingSelectedMuEl.first >= 0)
             break;
     }
 
     // Mu-Mu
     for(unsigned int imu1 = 0; imu1 < selectedMuons.size(); imu1++){
         for(unsigned int imu2 = imu1+1; imu2 < selectedMuons.size(); imu2++){
-            if( muons.charge[ selectedMuons[imu1] ] * muons.charge[ selectedMuons[imu2] ] < 0 ){
-                selectedLeadingMuMu = std::make_pair(imu1, imu2);
+            if( muons.charge[ selectedMuons[imu1] ] * muons.charge[ selectedMuons[imu2] ] < 0 && (muons.p4[ selectedMuons[imu1] ] + muons.p4[ selectedMuons[imu2] ]).M() > m_MllBaseCutSF){
+                leadingSelectedMuMu = std::make_pair(imu1, imu2);
+                tempSumPtMuMu = muons.p4[ selectedMuons[imu1] ].Pt() + muons.p4[ selectedMuons[imu2] ].Pt(); 
                 break;
             }
         }
-        if(selectedLeadingMuMu.first >= 0)
+        if(leadingSelectedMuMu.first >= 0)
             break;
     }
+
+    // Next, we define the selected lepton pair as the highest Sum(Pt) pair
+
+    if( (tempSumPtElEl > 0) && (tempSumPtElEl > tempSumPtElMu) && (tempSumPtElEl > tempSumPtMuEl) && (tempSumPtElEl > tempSumPtMuMu) ){
+      m_diLepton = { 
+        electrons.p4[leadingSelectedElEl.first] + electrons.p4[leadingSelectedElEl.second], 
+        std::make_pair<int, int>(leadingSelectedElEl.first, leadingSelectedElEl.second),
+        true, false, false, false
+        };
+    }else if( (tempSumPtElMu > 0) && (tempSumPtElMu > tempSumPtElEl) && (tempSumPtElMu > tempSumPtMuEl) && (tempSumPtElMu > tempSumPtMuMu) ){
+      m_diLepton = { 
+        electrons.p4[leadingSelectedElMu.first] + muons.p4[leadingSelectedElMu.second], 
+        std::make_pair<int, int>(leadingSelectedElMu.first, leadingSelectedElMu.second),
+        false, true, false, false
+        };
+    }else if( (tempSumPtMuEl > 0) && (tempSumPtMuEl > tempSumPtElEl) && (tempSumPtMuEl > tempSumPtElMu) && (tempSumPtMuEl > tempSumPtMuMu) ){
+      m_diLepton = { 
+        muons.p4[leadingSelectedMuEl.first] + electrons.p4[leadingSelectedMuEl.second], 
+        std::make_pair<int, int>(leadingSelectedMuEl.first, leadingSelectedMuEl.second),
+        false, false, true, false
+        };
+    }else if( (tempSumPtMuMu > 0) && (tempSumPtMuMu > tempSumPtElEl) && (tempSumPtMuMu > tempSumPtElMu) && (tempSumPtMuMu > tempSumPtMuEl) ){
+      m_diLepton = { 
+        muons.p4[leadingSelectedMuMu.first] + muons.p4[leadingSelectedMuMu.second], 
+        std::make_pair<int, int>(leadingSelectedMuMu.first, leadingSelectedMuMu.second),
+        false, false, false, true
+        };
+    }else{
+      m_diLepton = { LorentzVector(0,0,0,0), std::make_pair<int, int>(-1, -1), false, false, false, false };
+    }
+
+    
+
+
+    ///////////////////////////
+    //       JETS            //
+    ///////////////////////////
 
     // Save the jets that pass the cuts
 
@@ -103,6 +170,23 @@ void TTAnalyzer::analyze(const edm::Event& event, const edm::EventSetup& setup, 
         if( abs(jets.p4[ijet].Eta()) < m_jetEtaCut && jets.p4[ijet].Pt() > m_jetPtCut)
             selectedJets.push_back(ijet);
     }
+
+    ///////////////////////////
+    //       B-JETS          //
+    ///////////////////////////
+
+    ///////////////////////////
+    //    EVENT VARIABLES    //
+    ///////////////////////////
+
+    ///////////////////////////
+    //       TRIGGER         //
+    ///////////////////////////
+
+    ///////////////////////////
+    //       GEN INFO        //
+    ///////////////////////////
+
 }
 
 void TTAnalyzer::registerCategories(CategoryManager& manager, const edm::ParameterSet& config) {
