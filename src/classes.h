308d76470aee45efe0cdff69e9f77808f83fc95f
Sébastien <sebastien.wertz@uclouvain.be>
2015-10-14 09:34:58 +0200

    Added dictionaries

---
+++ src/classes.h
@@ -0,0 +1,19 @@
+#include <cp3_llbb/TTAnalysis/interface/Types.h>
+#include <vector>
+
+namespace {
+  struct dictionary {
+    TTAnalysis::BaseObject dummy;
+    std::vector<TTAnalysis::BaseObject> dummy2;
+    TTAnalysis::Lepton dummy3;
+    std::vector<TTAnalysis::Lepton> dummy4;
+    TTAnalysis::DiLepton dummy5;
+    std::vector<TTAnalysis::DiLepton> dummy6;
+    TTAnalysis::DiJet dummy7;
+    std::vector<TTAnalysis::DiJet> dummy8;
+    TTAnalysis::DiLepDiJet dummy9;
+    std::vector<TTAnalysis::DiLepDiJet> dummy10;
+    TTAnalysis::DiLepDiJetMet dummy11;
+    std::vector<TTAnalysis::DiLepDiJetMet> dummy12;
+  };
+}
