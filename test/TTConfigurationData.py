bad25caea0681790c5e8adfa39bc0898b3e92f6a
Sébastien Brochet <sebastien.brochet@cern.ch>
2015-11-10 17:01:05 +0100

    Relax trigger matching cuts

--- test/TTConfigurationData.py
+++ test/TTConfigurationData.py
@@ -31,8 +31,8 @@ process = Framework.create(True, eras.Run2_25ns, '74X_dataRun2_v2', cms.PSet(
             jetCSVv2M = cms.untracked.double(0.89),
             jetCSVv2T = cms.untracked.double(0.97),
 
-            hltDRCut = cms.untracked.double(0.05), # DeltaR cut for trigger matching
-            hltDPtCut = cms.untracked.double(0.1), #Delta(Pt)/Pt cut for trigger matching
+            hltDRCut = cms.untracked.double(0.3), # DeltaR cut for trigger matching
+            hltDPtCut = cms.untracked.double(0.5), #Delta(Pt)/Pt cut for trigger matching
             ),
         categories_parameters = cms.PSet(
             MllCutSF = cms.untracked.double(20),
--- test/TTConfigurationMC.py
+++ test/TTConfigurationMC.py
@@ -31,8 +31,8 @@ process = Framework.create(False, eras.Run2_25ns, '74X_mcRun2_asymptotic_v2', cm
             jetCSVv2M = cms.untracked.double(0.89),
             jetCSVv2T = cms.untracked.double(0.97),
 
-            hltDRCut = cms.untracked.double(0.05), # DeltaR cut for trigger matching
-            hltDPtCut = cms.untracked.double(0.1), #Delta(Pt)/Pt cut for trigger matching
+            hltDRCut = cms.untracked.double(0.3), # DeltaR cut for trigger matching
+            hltDPtCut = cms.untracked.double(0.5), #Delta(Pt)/Pt cut for trigger matching
             ),
         categories_parameters = cms.PSet(
             MllCutSF = cms.untracked.double(20),
@@ -75,4 +75,4 @@ process.source.fileNames = cms.untracked.vstring(
         'file:/nfs/scratch/fynu/swertz/CMSSW_7_4_15/src/cp3_llbb/TTAnalysis/test/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_miniAODv2_oneFile.root'
         )
 
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1000))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
